9c9af628cb7dcac157d7c3349d0057552912b519
Mihir Abhale <166193326+Mihirabhale@users.noreply.github.com>
2026-07-04 23:44:45 +0530

    Time: 2 ms (50.94%), Space: 8.6 MB (37.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;  // negative numbers are never palindromes
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return reversed == x;
+    }
+};
