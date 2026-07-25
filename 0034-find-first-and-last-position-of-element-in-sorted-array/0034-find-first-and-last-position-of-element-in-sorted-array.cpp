dbf1ab8e09663f041c62ee59044942f39e25754f
Mihir Abhale <166193326+Mihirabhale@users.noreply.github.com>
2026-07-25 19:42:37 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.64%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findFirst(nums, target);
+        int last  = findLast(nums, target);
+        return {first, last};
+    }
+
+private:
+    int findFirst(vector<int>& nums, int target) {
+        int left  = 0;
+        int right = (int)nums.size() - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;   // record and keep searching left
+                right  = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int left   = 0;
+        int right  = (int)nums.size() - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;   // record and keep searching right
+                left   = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
